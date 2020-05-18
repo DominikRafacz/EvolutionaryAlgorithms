b495b8f3d65f3f8be9acd0ae92484bcaa972ea20
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-05-19 00:17:53 +0200

    implemented random one point mutation

---
+++ src/main/java/dr/mio/evo/lab/mutation/MutationDescOnePointGaussian.java
@@ -0,0 +1,38 @@
+package dr.mio.evo.lab.mutation;
+
+import dr.mio.evo.alg.State;
+import dr.mio.evo.alg.desc.MutationDesc;
+import dr.mio.evo.alg.genotype.GenotypeEuclidean;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+public class MutationDescOnePointGaussian implements MutationDesc<GenotypeEuclidean> {
+    private final double probability;
+    private final double mean;
+    private final double deviation;
+    private final Random random = new Random();
+
+    public MutationDescOnePointGaussian(double probability, double mean, double deviation) {
+        this.probability = probability;
+        this.mean = mean;
+        this.deviation = deviation;
+    }
+
+    public MutationDescOnePointGaussian(double probability) {
+        this(probability, 0, 1);
+    }
+
+    /*
+    * Losowo wybranym osobnikom dodajemy losowe mutacje z rozkładu normalnego
+    * */
+    @Override
+    public void performMutating(@NotNull State<GenotypeEuclidean> state) {
+        var dimensions = state.getPopulation().get(0).getDimensions();
+        state.getPopulation()
+                .forEach(genotype -> {
+                    if (random.nextDouble() < probability)
+                        genotype.mutateAt(random.nextInt(dimensions), random.nextGaussian() * deviation + mean);
+                });
+    }
+}
