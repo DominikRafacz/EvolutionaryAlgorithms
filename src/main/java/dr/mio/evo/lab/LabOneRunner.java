3130948bcbc59567ff5c91ee1d808f4abeaa6688
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-05-19 00:18:21 +0200

    added first lab

--- src/main/java/dr/mio/evo/lab/LabOneRunner.java
+++ src/main/java/dr/mio/evo/lab/LabOneRunner.java
@@ -1,7 +1,51 @@
 package dr.mio.evo.lab;
 
+import dr.mio.evo.alg.desc.EvolutionaryAlgorithmDesc;
+import dr.mio.evo.alg.desc.crossing.CrossingDescRandomPairs;
+import dr.mio.evo.alg.genotype.GenotypeEuclidean;
+import dr.mio.evo.alg.population.PopulationDescEuclideanSpace;
+import dr.mio.evo.alg.target.Targets;
+import dr.mio.evo.lab.criterion.CriterionDescFixedIterations;
+import dr.mio.evo.lab.mutation.MutationDescOnePointGaussian;
+
+import java.util.stream.IntStream;
+
 public class LabOneRunner {
     public static void main(String[] args) {
+        // tworzymy algorytm ewolucyjny
+        var algorithm = EvolutionaryAlgorithmDesc.<GenotypeEuclidean>builder()
+                // każdy osobnik z naszej populacji będzie punktem w 3-wymiarowej przestrzeni o maks./min. wartości wymiaru +-10; populacja będzie liczyć 1000 osobnikóœ
+                .populationDesc(new PopulationDescEuclideanSpace(3, 1000, -10, 10))
+                // naszym celem jest minimalizacja zadanej funckji
+                .targetDesc(Targets.euclideanFunction(x -> x.at(0) * x.at(0) + x.at(1) * x.at(1) + 2 * x.at(2) * x.at(2)))
+                // krzyżywane pary będą dobierane losowo
+                .crossingDesc(new CrossingDescRandomPairs())
+                // mutacja będzie zachodziła u ok 20% osobników i będzie dotyczyła jednej współrzędnej
+                .mutationDesc(new MutationDescOnePointGaussian(0.2))
+                // wykonanych zostanie 1000 iteracji
+                .criterionDesc(new CriterionDescFixedIterations(1000))
+                // budujemy wzorzec algorytmu
+                .build()
+                // uzyskujemy algorytm
+                .getAlgorithm();
+        //uruchamiamy algorytm - to kluczowa funkcja całego programu
+        algorithm.run();
+        //wypisujemy wyniki
+        System.out.println(algorithm.getResults().toString());
+        // bestGenotype=[0.056271070833511336, -0.2000371245861285, 1.6493333649530733], value=0.0019387734117407693
+
+        // analogicznie jak wyżej, zmienia się funkcja celu
+        algorithm = EvolutionaryAlgorithmDesc.<GenotypeEuclidean>builder()
+                .populationDesc(new PopulationDescEuclideanSpace(5, 5000, -5.12, 5.12))
+                .targetDesc(Targets.euclideanFunction(x -> 50 + IntStream.range(0, 5).mapToDouble(i -> x.at(i) * x.at(i) - 10 * Math.cos(2 * Math.PI * x.at(i))).sum()))
+                .crossingDesc(new CrossingDescRandomPairs())
+                .mutationDesc(new MutationDescOnePointGaussian(0.2))
+                .criterionDesc(new CriterionDescFixedIterations(1000))
+                .build()
+                .getAlgorithm();
+        algorithm.run();
+        System.out.println(algorithm.getResults().toString());
+
 
     }
 }
