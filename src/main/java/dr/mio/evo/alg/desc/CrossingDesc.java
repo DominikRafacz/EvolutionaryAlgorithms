ff3d955b9bd6d2ab7b7a4566f645244797187480
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-05-26 08:48:56 +0200

    more independence!

--- src/main/java/dr/mio/evo/alg/desc/CrossingDesc.java
+++ src/main/java/dr/mio/evo/alg/desc/CrossingDesc.java
@@ -1,3 +1,7 @@
+// Oświadczam, że niniejsza praca stanowiąca podstawę do uznania osiągnięcia efektów uczenia się z przedmiotu Metody Inteligencji Obliczeniowej w Analizie Danych została wykonana przeze mnie samodzielnie.
+// Dominik Rafacz
+// 291128
+
 // Potwierdzam samodzielność powyższej pracy oraz niekorzystanie przeze mnie z niedozwolonych źródeł.
 // Dominik Rafacz
 
--- src/main/java/dr/mio/evo/alg/util/RectangleTemplate.java
+++ src/main/java/dr/mio/evo/alg/util/RectangleTemplate.java
@@ -1,3 +1,11 @@
+// Oświadczam, że niniejsza praca stanowiąca podstawę do uznania osiągnięcia efektów uczenia się z przedmiotu Metody Inteligencji Obliczeniowej w Analizie Danych została wykonana przeze mnie samodzielnie.
+// Dominik Rafacz
+// 291128
+
+// Potwierdzam samodzielność powyższej pracy oraz niekorzystanie przeze mnie z niedozwolonych źródeł.
+// Dominik Rafacz
+
+
 package dr.mio.evo.alg.util;
 
 import lombok.AllArgsConstructor;
