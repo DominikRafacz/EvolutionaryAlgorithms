685c3ef9cc071c84485a17e226a651e0461155c5
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-06-02 08:29:22 +0200

    added declaration of independence again

--- src/main/java/dr/mio/evo/alg/mating/MatingDescNone.java
+++ src/main/java/dr/mio/evo/alg/mating/MatingDescNone.java
@@ -1,3 +1,10 @@
+// Oświadczam, że niniejsza praca stanowiąca podstawę do uznania osiągnięcia efektów uczenia się z przedmiotu Metody Inteligencji Obliczeniowej w Analizie Danych została wykonana przeze mnie samodzielnie.
+// Dominik Rafacz
+// 291128
+
+// Potwierdzam samodzielność powyższej pracy oraz niekorzystanie przeze mnie z niedozwolonych źródeł.
+// Dominik Rafacz
+
 package dr.mio.evo.alg.mating;
 
 import dr.mio.evo.alg.State;
--- src/main/java/dr/mio/evo/alg/space/SpaceDescNEAT.java
+++ src/main/java/dr/mio/evo/alg/space/SpaceDescNEAT.java
@@ -1,3 +1,10 @@
+// Oświadczam, że niniejsza praca stanowiąca podstawę do uznania osiągnięcia efektów uczenia się z przedmiotu Metody Inteligencji Obliczeniowej w Analizie Danych została wykonana przeze mnie samodzielnie.
+// Dominik Rafacz
+// 291128
+
+// Potwierdzam samodzielność powyższej pracy oraz niekorzystanie przeze mnie z niedozwolonych źródeł.
+// Dominik Rafacz
+
 package dr.mio.evo.alg.space;
 
 import dr.mio.evo.alg.desc.SpaceDesc;
